b9cbeb844dbad8f27c6b16ea935dda901a60eba4
rrankawat <rakesh.rankawat@outlook.com>
2019-08-30 17:00:50 +0530

    1.0

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './components/counter';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
